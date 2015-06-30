532ada6c8ed9eade76341dc39c99f209192d77e5
Dan Ungureanu <udan1107@gmail.com>
2015-06-30 22:17:29 +0300

    Miscellaneous fixes.

--- tests/Fragments/WhereKeywordTest.php
+++ tests/Fragments/WhereKeywordTest.php
@@ -13,6 +13,6 @@ class WhereKeywordTest extends TestCase
     public function testParse()
     {
         $fragment = WhereKeyword::parse(new Parser(), $this->getTokensList('/* id = */ id = 10'));
-        $this->assertEquals($fragment[0]->condition, 'id = 10');
+        $this->assertEquals($fragment[0]->expr, 'id = 10');
     }
 }
