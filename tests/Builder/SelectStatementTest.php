42c34ddd1fee261da43331e855c80bc432d66247
Dan Ungureanu <udan1107@gmail.com>
2015-10-10 15:37:25 +0300

    Avoid building a field multiple times if clause has synonyms.

---
+++ tests/Builder/SelectStatementTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SqlParser\Tests\Builder;
+
+use SqlParser\Parser;
+
+use SqlParser\Tests\TestCase;
+
+class SelectStatementTest extends TestCase
+{
+
+    public function testBuilder()
+    {
+        $query = 'SELECT * FROM t1 LEFT JOIN (t2, t3, t4) '
+            . 'ON (t2.a=t1.a AND t3.b=t1.b AND t4.c=t1.c)';
+
+        $parser = new Parser($query);
+        $stmt = $parser->statements[0];
+
+        $this->assertEquals(
+            'SELECT  * FROM t1 LEFT JOIN (t2, t3, t4) '
+            . 'ON (t2.a=t1.a AND t3.b=t1.b AND t4.c=t1.c) ',
+            $stmt->build()
+        );
+    }
+}
