47d03c5dc614939b2df057b984544635ae6217db
Dan Ungureanu <udan1107@gmail.com>
2015-12-18 15:56:43 +0200

    Allow NULL in expressions.

--- src/Statement.php
+++ src/Statement.php
@@ -170,7 +170,6 @@ abstract class Statement
                 if (!empty($built[$field])) {
                     continue;
                 }
-
                 $built[$field] = true;
             }
 
