33f62b8de25ad847b6e3948a87d17163fd090492
Dan Ungureanu <udan1107@gmail.com>
2016-02-21 15:15:20 +0200

    Misc: Fix failing tests.
    Signed-off-by: Dan Ungureanu <udan1107@gmail.com>

--- tests/Builder/AlterStatementTest.php
+++ tests/Builder/AlterStatementTest.php
@@ -9,20 +9,15 @@ use SqlParser\Tests\TestCase;
 class AlterStatementTest extends TestCase
 {
 
-    public function testBuilderView()
+    public function testBuilder()
     {
         $query = 'ALTER TABLE `actor` ' .
             'ADD PRIMARY KEY (`actor_id`), ' .
-            'ADD KEY `idx_actor_last_name` (`last_name`);';
+            'ADD KEY `idx_actor_last_name` (`last_name`)';
 
         $parser = new Parser($query);
         $stmt = $parser->statements[0];
 
-        $this->assertEquals(
-            'ALTER  TABLE `actor` ' .
-            'ADD PRIMARY KEY (`actor_id`), ' .
-            'ADD KEY `idx_actor_last_name` (`last_name`)',
-            $stmt->build()
-        );
+        $this->assertEquals($query, $stmt->build());
     }
 }
