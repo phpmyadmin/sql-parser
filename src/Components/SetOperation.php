14777f12fa5c089888dea54b8ad2c782e00567d9
Michal Čihař <michal@cihar.com>
2017-01-03 12:31:52 +0100

    Properly report missing expression error
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- src/Components/SetOperation.php
+++ src/Components/SetOperation.php
@@ -106,6 +106,7 @@ class SetOperation extends Component
                     )
                 );
                 if ($tmp == null) {
+                    $parser->error(__('Missing expression.'), $token);
                     break;
                 }
                 $expr->column = trim($expr->column);
