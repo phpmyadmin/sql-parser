c7e99ad4b4843399625283ac72c5e58db36f04fc
Putinas Piliponis <putinas.piliponis@ruduo.net>
2017-10-10 14:20:55 +0300

    Update CreateStatement.php
    I think there is mistake in code as well:
    
                        if ($brackets === 0) {
                            $this->$field = trim($this->$field);
                            $field = null;
                        }

--- src/Statements/CreateStatement.php
+++ src/Statements/CreateStatement.php
@@ -515,7 +515,7 @@ class CreateStatement extends Statement
                     }
 
                     // Building the expression used for partitioning.
-                    $this->$field .= ($token->type === Token::TYPE_WHITESPACE) ? ' ' : $token->token;
+                    $field .= ($token->type === Token::TYPE_WHITESPACE) ? ' ' : $token->token;
 
                     // Last bracket was read, the expression ended.
                     // Comparing with `0` and not `false`, because `false` means
