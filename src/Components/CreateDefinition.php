60d101d3ffab6c18a4aaa22614bfa65e10da0162
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2021-09-16 03:51:04 +0200

    Fixing validation exception when using enforced option
    Signed-off-by: Fawzi E. Abdulfattah <iifawzie@gmail.com>

--- src/Components/CreateDefinition.php
+++ src/Components/CreateDefinition.php
@@ -85,8 +85,8 @@ class CreateDefinition extends Component
             'expr',
             array('parenthesesDelimited' => true),
         ),
-
         'INVISIBLE' => 13,
+        'ENFORCED' => 14,
         // Common entries.
         //
         // NOTE: Some of the common options are not in the same order which
