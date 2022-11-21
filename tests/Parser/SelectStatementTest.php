0d4e00ca0f277276c94cfcab4acc7f8d93f196cd
iifawzi <iifawzie@gmail.com>
2022-11-21 17:54:13 +0200

    Adding complex select test
    Signed-off-by: iifawzi <iifawzie@gmail.com>

--- tests/Parser/SelectStatementTest.php
+++ tests/Parser/SelectStatementTest.php
@@ -42,6 +42,7 @@ class SelectStatementTest extends TestCase
             ['parser/parseSelect10'],
             ['parser/parseSelect11'],
             ['parser/parseSelect12'],
+            ['parser/parseSelect13'],
             ['parser/parseSelectErr1'],
             ['parser/parseSelectErr2'],
             ['parser/parseSelectNested'],
