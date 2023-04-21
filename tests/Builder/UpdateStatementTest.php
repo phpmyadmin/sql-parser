4ad634b288e0aaf820fc85e32892671b26959497
William Desportes <williamdes@wdes.fr>
2023-04-21 22:20:56 +0200

    Fix #260 - Update test data
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- tests/Builder/UpdateStatementTest.php
+++ tests/Builder/UpdateStatementTest.php
@@ -17,14 +17,14 @@ class UpdateStatementTest extends TestCase
         );
         $stmt = $parser->statements[0];
         $this->assertEquals(
-            'UPDATE user AS `u` SET ud.ip = \'33\' WHERE u.id = 1',
+            'UPDATE user AS `u` LEFT JOIN user_detail AS `ud` ON u.id = ud.user_id SET ud.ip = \'33\' WHERE u.id = 1',
             $stmt->build()
         );
         /* Assertion 2 */
         $parser = new Parser('update user u join user_detail ud on u.id = ud.user_id set ud.ip =\'33\' where u.id = 1');
         $stmt = $parser->statements[0];
         $this->assertEquals(
-            'UPDATE user AS `u` SET ud.ip = \'33\' WHERE u.id = 1',
+            'UPDATE user AS `u` JOIN user_detail AS `ud` ON u.id = ud.user_id SET ud.ip = \'33\' WHERE u.id = 1',
             $stmt->build()
         );
         /* Assertion 3 */
@@ -33,7 +33,7 @@ class UpdateStatementTest extends TestCase
         );
         $stmt = $parser->statements[0];
         $this->assertEquals(
-            'UPDATE user AS `u` SET ud.ip = \'33\' WHERE u.id = 1',
+            'UPDATE user AS `u` INNER JOIN user_detail AS `ud` ON u.id = ud.user_id SET ud.ip = \'33\' WHERE u.id = 1',
             $stmt->build()
         );
     }
