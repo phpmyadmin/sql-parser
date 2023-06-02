1eaaa0cb9786234ab5e8828199df56a47c78ae8e
Kamil Tekiela <tekiela246@gmail.com>
2023-06-02 14:52:36 +0100

    Add less specific Psalm-var
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- src/Contexts/ContextMariaDb100000.php
+++ src/Contexts/ContextMariaDb100000.php
@@ -27,6 +27,7 @@ class ContextMariaDb100000 extends Context
      *      Token::FLAG_KEYWORD_FUNCTION
      *
      * @var array<string,int>
+     * @psalm-var non-empty-array<string,Token::FLAG_KEYWORD_*|int>
      * @phpstan-var non-empty-array<non-empty-string,Token::FLAG_KEYWORD_*|int>
      */
     public static $keywords = [
