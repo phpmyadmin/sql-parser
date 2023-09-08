be2ca976c9095e5a453af6bbecb0c8dd82c6eae8
Kamil Tekiela <tekiela246@gmail.com>
2023-09-08 16:46:49 +0100

    Update UtfStringBench.php
    Signed-off-by: Kamil Tekiela <tekiela246@gmail.com>

--- tests/benchmarks/UtfStringBench.php
+++ tests/benchmarks/UtfStringBench.php
@@ -18,8 +18,7 @@ class UtfStringBench
      * @Iterations(20)
      * @Revs(4)
      * @OutputTimeUnit("milliseconds")
-     * @Assert("mode(variant.time.avg) < 100 milliseconds +/- 10%")
-     * @Assert("mode(variant.time.avg) > 30 milliseconds +/- 10%")
+     * @Assert("mode(variant.time.avg) < 40 milliseconds +/- 10%")
      */
     public function benchBuildUtfString(): void
     {
@@ -33,5 +32,26 @@ class UtfStringBench
     {
         $contentsPath = __DIR__ . '/../../LICENSE.txt';
         $this->testContents = (string) file_get_contents($contentsPath);
+    }
+
+    /**
+     * @Iterations(20)
+     * @Revs(4)
+     * @OutputTimeUnit("microseconds")
+     * @Assert("mode(variant.time.avg) < 120 microseconds +/- 10%")
+     */
+    public function benchUtfStringRandomAccessWithUnicode(): void
+    {
+        $text = 'abcdefghijklmnopqrstuvwxyz
+        áéíóúýěřťǔǐǒǎšďȟǰǩľžčǚň
+        🦋😄😃😀😊😉😍😘😚😗😂👿😮😨😱😠😡😤😖😆😋👯
+        P\xf8\xed\xb9ern\xec \xbelu\xbbou\xe8k\xfd k\xf3d \xfap\xecl \xef\xe1belsk\xe9 k\xf3dy
+        xℤⅿↈⅬ⅀ↆℜℝ⅗ℾ℧ⅰℓⅯⅵⅣ⅒21⅞';
+
+        $str1 = new UtfString($text);
+        $str1->offsetGet(10);
+        $str1->offsetGet(100);
+        $str1->offsetGet(20);
+        $str1->offsetGet(0);
     }
 }
